78fb4fed08275ea905b8b3a07ab3db817d00b8cf
stkrolikiewicz <st.krolikiewicz@gmail.com>
2023-10-16 17:15:01 +0200

    feat: add ColorMode change funcionality

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,11 +1,11 @@
 import '@fontsource/raleway/400.css';
 import '@fontsource/open-sans/700.css';
-import {ChakraProvider} from '@chakra-ui/react';
+import {ChakraProvider, localStorageManager} from '@chakra-ui/react';
 import theme from '@/styles/theme';
 
 export default function App({Component, pageProps}) {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider colorModeManager={localStorageManager} theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   );
--- client/src/styles/theme.js
+++ client/src/styles/theme.js
@@ -1,6 +1,8 @@
 import {extendTheme} from '@chakra-ui/react';
 
 const config = {
+  initialColorMode: 'system',
+  useSystemColorMode: false,
   fonts: {
     heading: `'Open Sans', sans-serif`,
     body: `'Raleway', sans-serif`,
